70a37d4819e8331c324dbf0e65ec5498162b5145
Kurichi <yuuyapp080@gmail.com>
2023-01-19 01:14:07 +0900

    tutorial 2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
-<script setup lang="ts">
+<script lang="ts">
+export default {
+  data() {
+    return {
+      title: "My New Vue Title",
+      message: "Hello World",
+      textStatus: {
+        red: true,
+      },
+      input: {
+        givenName: "",
+        familyName: "",
+        isMember: true,
+      },
+      users: [
+        {
+          givenName: "John",
+          familyName: "Smith",
+          isMember: true,
+        },
+        {
+          givenName: "Taro",
+          familyName: "Shinjuku",
+          isMember: false,
+        },
+        {
+          givenName: "Hanako",
+          familyName: "Shibuya",
+          isMember: true,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    addUser() {
+      this.users.push(this.input);
+      this.input = {
+        givenName: "",
+        familyName: "",
+        isMember: true,
+      };
+    },
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1 :title="message" :class="textStatus">{{ title }}</h1>
+  <input type="text" v-model="input.givenName" />
+  <input type="text" v-model="input.familyName" />
+  <input type="checkbox" v-model="input.isMember" />
+  <button @click="addUser">ユーザの追加</button>
+
+  <h2>ユーザのデータ</h2>
+  <div v-for="user in users">
+    <p>Name: {{ user.givenName + " " + user.familyName }}</p>
+    <p v-if="user.isMember">メンバーです</p>
+    <p v-else="user.isMember">メンバーではありません</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.red {
+  color: red;
 }
 </style>
